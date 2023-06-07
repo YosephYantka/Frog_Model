15533db63e77aef212636ebb7344cfd466fe8e06
YosephYantka <josephjantke365@outlook.com.au>
2023-06-07 09:58:50 +0930

    07/06

--- spectrogram.py
+++ spectrogram.py
@@ -9,39 +9,18 @@ from torchaudio.utils import download_asset
 import numpy
 matplotlib.use('qt5agg')
 torch.random.manual_seed(0)
-SAMPLE_SPEECH = '/home/nottom/Documents/LinuxProject/BAR4_slices/0_4_BAR4_20210709_234000.wav'
-filename = "0_4_BAR4_20210709_234000.wav"
-#SAMPLE_SPEECH = "/home/nottom/Documents/LinuxProject/example.wav"
 
-def plot_waveform(waveform, sr, title="Waveform"):
-    waveform = waveform.numpy()
+SAMPLE_SPEECH = '/home/nottom/Documents/LinuxProject/BAR4_slices_1/BAR4_20210709_234000.wav'
+filename = str(SAMPLE_SPEECH[-24:-1])
 
-    num_channels, num_frames = waveform.shape
-    time_axis = torch.arange(0, num_frames) / sr
-
-    figure, axes = plt.subplots(num_channels, 1)
-    axes.plot(time_axis, waveform[0], linewidth=1)
-    axes.grid(True)
-    figure.suptitle(title)
-    plt.show(block=True)
-
-def plot_spectrogram(specgram, title=None, ylabel="freq_bin"):
+def save_spectrogram(specgram, title=None, ylabel="freq_bin"):
     fig, axs = plt.subplots(1, 1)
     axs.set_title(title or "Spectrogram (db)")
     axs.set_ylabel(ylabel)
     axs.set_xlabel("frame")
     im = axs.imshow(librosa.power_to_db(specgram), origin="lower", aspect="auto")
     fig.colorbar(im, ax=axs)
-    plt.show(block=True)
-    plt.savefig("/home/nottom/Documents/LinuxProject/misc/spec1.png")
-
-def plot_fbank(fbank, title=None):
-    fig, axs = plt.subplots(1, 1)
-    axs.set_title(title or "Filter bank")
-    axs.imshow(fbank, aspect="auto")
-    axs.set_ylabel("frequency bin")
-    axs.set_xlabel("mel bin")
-    plt.show(block=True)
+    plt.savefig("/home/nottom/Documents/LinuxProject/misc/" + str(title) + '.png')
 
 #TO PLOT THE SPECTROGRAM:
 SPEECH_WAVEFORM, SAMPLE_RATE = torchaudio.load(SAMPLE_SPEECH)
@@ -58,9 +37,5 @@ spectrogram = T.Spectrogram(
     power=2.0,)
 # Perform transform
 spec = spectrogram(SPEECH_WAVEFORM)
-plot_spectrogram(spec[0], title="Spectrogram " + str(filename))
-#plt.savefig("/home/nottom/Documents/LinuxProject/misc/spec1.png")
+save_spectrogram(spec[0], title="Spectrogram_" + str(filename))
 
-
-import matplotlib
-matplotlib.get_backend()
--- training_data_pipeline.py
+++ training_data_pipeline.py
@@ -1,21 +1,28 @@
+# this file chunks specified wav files, and creates and exports text documents and spectrograms for each chunk
 import torch
+import torchaudio
+import torchaudio.functional as F
+import torchaudio.transforms as T
 import re
-import csv
-import wave
-import contextlib
-import numpy as np
-import pprint
 from pydub import AudioSegment
 import math
+import matplotlib
+import librosa
+import matplotlib.pyplot as plt
+import os
 
-#set file and folder(s)
+matplotlib.use('qt5agg')
+torch.random.manual_seed(0)
+
+# set file and folder(s)
 chunk_folder = '/home/nottom/Documents/LinuxProject/BAR4_slices'
-wav_file = 'BAR4_20210709_234000.wav'
-#^this file must be in the chunk_folder
-filename = 'BAR4_20210709_234000'
+chunk_file = 'BAR4_20210709_234000.wav'
+# ^this file must be in the chunk_folder
+#wav_file = '/home/nottom/Documents/LinuxProject/BAR4_slices_1/BAR4_20210709_234000.wav'
+filename = str(chunk_file[0:-4])
 audacity_labels = '/home/nottom/Documents/LinuxProject/labels.txt'
 
-#Split audio file into 4 second clips
+# Split audio file into 4 second clips
 class SplitWavAudioMubin():
     def __init__(self, folder, filename):
         self.folder = folder
@@ -36,13 +43,13 @@ class SplitWavAudioMubin():
     def multiple_split(self, sec_per_split):
         total_secs = math.ceil(self.get_duration())
         for i in range(0, total_secs, sec_per_split - 1):
-            split_fn = str(i) + '_' + str(i+4) + '_' + self.filename
+            split_fn = str(i) + '_' + str(i + 4) + '_' + self.filename
             self.single_split(i, i + sec_per_split, split_fn)
             print(str(i) + ' Done')
             if i == total_secs - sec_per_split:
                 print('All splited successfully')
 
-split_wav = SplitWavAudioMubin(chunk_folder, wav_file)
+split_wav = SplitWavAudioMubin(chunk_folder, chunk_file)
 split_wav.multiple_split(sec_per_split=4)
 
 # for creating list of all dummy annotations
@@ -54,31 +61,32 @@ mylist = ([s for s in mylist if s != '\\'])
 mylist = ([s for s in mylist if s != ''])
 mylist = [float(x) for x in mylist]
 
-#make a list of lists for annotations:
+# make a list of lists for annotations:
 def split(list_a, chunk_size):
-  for i in range(0, len(list_a), chunk_size):
-    yield list_a[i:i + chunk_size]
+    for i in range(0, len(list_a), chunk_size):
+        yield list_a[i:i + chunk_size]
+
 chunk_size = 5
 annotations_list = list(split(mylist, chunk_size))
 
-#create list of fours using length of recording
+# create list of fours using length of recording
 duration = split_wav.duration
-segments = round(duration/3)
+segments = round(duration / 3)
 fours = torch.linspace(4, duration, segments)
-print(fours)
 
-#create a corresponding dummy zeros tensor for the entire sound file
+# create a corresponding dummy zeros tensor for the entire sound file
 annotation = torch.zeros(segments, 4)
 
-#Create a one-hot encoding tensor for each 4 second chunk
-#THIS ONE WORKS (for some reason I have to write this into two pieces of code!!)
-for t0,t1,id,f0,f1 in annotations_list:
-    annotation[:, int(id)] = torch.logical_or(annotation[:, int(id)], torch.logical_and(fours - 0.5 > t0, fours - 3.5 < t1))
+# Create a one-hot encoding tensor for each 4 second chunk
+# THIS ONE WORKS (for some reason I have to write this into two pieces of code!!)
+for t0, t1, id, f0, f1 in annotations_list:
+    annotation[:, int(id)] = torch.logical_or(annotation[:, int(id)],
+                                              torch.logical_and(fours - 0.5 > t0, fours - 3.5 < t1))
     annotation[:, 0] = torch.logical_and(fours > 0, fours > 0)
-for t0,t1,id,f0,f1 in annotations_list:
+for t0, t1, id, f0, f1 in annotations_list:
     annotation[:, 0] = torch.logical_and(torch.sum(annotation, 1) == 0, fours > 0)
 
-#write a text file for each 'chunk'
+# write a text file for each 'chunk'
 x = 0
 y = 4
 for chunk in annotation:
@@ -88,5 +96,43 @@ for chunk in annotation:
     encoding_string = f'{encoding[0]}, {encoding[1]}, {encoding[2]}, {encoding[3]}'
     with open('/home/nottom/Documents/LinuxProject/BAR4_txt/' + name + '.txt', 'x') as f:
         f.write(encoding_string)
-    x = x+3
-    y = y+3
+    x = x + 3
+    y = y + 3
+
+#class for creating spectrogram
+
+def save_spectrogram(specgram, title=None, ylabel="freq_bin"):
+    fig, axs = plt.subplots(1, 1)
+    axs.set_title(title or "Spectrogram (db)")
+    axs.set_ylabel(ylabel)
+    axs.set_xlabel("frame")
+    im = axs.imshow(librosa.power_to_db(specgram), origin="lower", aspect="auto")
+    fig.colorbar(im, ax=axs)
+    plt.savefig("/home/nottom/Documents/LinuxProject/BAR4_spectrograms/" + str(title) + '.png')
+    plt.close()
+
+#iterate through each file in the chunk folder
+directory = chunk_folder
+x = 0
+y = 4
+for file in os.listdir(chunk_folder):
+    f = os.path.join(directory, file)
+    SPEECH_WAVEFORM, SAMPLE_RATE = torchaudio.load(f)
+    n_fft = 1024
+    win_length = None
+    hop_length = 512
+    # Define transform
+    spectrogram = T.Spectrogram(
+        n_fft=n_fft,
+        win_length=win_length,
+        hop_length=hop_length,
+        center=True,
+        pad_mode="reflect",
+        power=2.0, )
+    # Perform transform
+    spec = spectrogram(SPEECH_WAVEFORM)
+    save_spectrogram(spec[0], title=str(file))
+    #save_spectrogram(spec[0], title= str(x) + '_' + str(y) + '_' + "spectrogram_" + str(filename))
+    x = x + 3
+    y = y + 3
+
